374ef6bb1b99e9c72684106e3c4027213d8b0333
eggls6 <siegfried.eggl@gmail.com>
2022-06-27 14:21:39 -0500

    bugfix in __init__.py

--- heliolinc3d/__init__.py
+++ heliolinc3d/__init__.py
@@ -5,11 +5,6 @@ from .propagate import *
 from .ephemeris import *
 from .filterclusters import *
 from .heliolinc3d import *
-<<<<<<< HEAD
-# from .performancecheck import *
-=======
-from .performancecheck import *
->>>>>>> 415dda648654555f762711aabcc515ee0b3832b0
 from .plot import *
 from .pandas_utils import *
 from .utility import *
