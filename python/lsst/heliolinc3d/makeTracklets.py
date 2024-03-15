6d72808298b919afc6a033f128bfe70204497460
Mario Juric <mjuric@sdfrome002.sdf.slac.stanford.edu>
2024-03-15 11:25:49 -0700

    WIP: Initial pipeline

--- python/lsst/heliolinc3d/makeTracklets.py
+++ python/lsst/heliolinc3d/makeTracklets.py
@@ -2,41 +2,47 @@ import heliohypy
 import lsst.pex.config
 import lsst.pipe.base
 from lsst.pipe.base import connectionTypes
+import solarsyst_dyn_geo as solardg
+import pandas as pd
+import numpy as np
 
 class MakeTrackletsConnections(lsst.pipe.base.PipelineTaskConnections,
-                               dimensions=("instrument")):
+                               dimensions=["instrument"]):
     diaSourceTable = connectionTypes.Input(
         doc="Table of unattributed sources",
-        dimensions=("instrument"),
+        dimensions=["instrument"],
         storageClass="DataFrame",
         name = "sspDiaSourceInputs"
     )
     visitTable = connectionTypes.Input(
         doc="visit stats plus observer coordinates",
-        dimensions=("instrument"),
+        dimensions=["instrument"],
         storageClass="DataFrame",
         name = "sspVisitInputs"
     )
     trackletSources = connectionTypes.Output(
         doc="sources that got included in tracklets",
-        dimensions=("instrument"),
+        dimensions=["instrument"],
         storageClass="DataFrame",
         name = "sspTrackletSources"
     )
     tracklets = connectionTypes.Output(
         doc="summary data for tracklets",
-        dimensions=("instrument"),
+        dimensions=["instrument"],
         storageClass="DataFrame",
         name = "sspTracklets"
-     )
-     trk2det = connectionTypes.Output(
+    )
+    trk2det = connectionTypes.Output(
         doc="indices connecting tracklets to trackletSources",
-        dimensions=("instrument"),
+        dimensions=["instrument"],
         storageClass="DataFrame",
         name = "sspTrackletToSource"
-     )
+    )
 
-class MakeTrackletsConfig(lsst.pex.config.Config):
+def getImageTimeTol():
+  return 2./(24.*3600.)
+
+class MakeTrackletsConfig(lsst.pipe.base.PipelineTaskConfig, pipelineConnections=MakeTrackletsConnections):
   mintrkpts = lsst.pex.config.Field(
       dtype=int,
       default=2,
@@ -44,7 +50,7 @@ class MakeTrackletsConfig(lsst.pex.config.Config):
       )
   imagetimetol = lsst.pex.config.Field(
       dtype=float,
-      default=getImageTimeTol()
+      default=getImageTimeTol(),
       doc="Tolerance for matching image time, in days: e.g. 1 second"
       )
   maxvel = lsst.pex.config.Field(
@@ -98,6 +104,19 @@ class MakeTrackletsConfig(lsst.pex.config.Config):
       doc="Prints monitoring output."
       )
 
+
+def df_to_numpy(df, dtype):
+    sa = np.zeros(len(df), dtype=dtype)
+    for col in df.columns:
+        sa[col] = df[col]
+    return sa
+
+def det_to_numpy(df):
+    return df_to_numpy(df, dtype=solardg.hldet)
+
+def vis_to_numpy(df):
+    return df_to_numpy(df, dtype=solardg.hlimage)
+
 class MakeTrackletsTask(lsst.pipe.base.PipelineTask):
     ConfigClass = MakeTrackletsConfig
     _DefaultName = "makeTracklets"
@@ -107,12 +126,23 @@ class MakeTrackletsTask(lsst.pipe.base.PipelineTask):
            here
         """
 
-        trackout = heliohypy.makeTracklets(self.config, diaSourceTable, visitTable)
+        # copy all config parameters from the Task's config object
+        # to heliolinc's native config object.
+        config = heliohypy.MakeTrackletsConfig()
+        allvars = [ item for item in dir(heliohypy.MakeTrackletsConfig) if not item.startswith("__") ]
+        for var in allvars:
+            setattr(config, var, getattr(self.config, var))
+
+        # convert dataframes to numpy array with dtypes that heliolinc expects
+        diaSourceTable = det_to_numpy(diaSourceTable)
+        visitTable     = vis_to_numpy(visitTable)
+
+        (dets, tracklets, trac2det) = heliohypy.makeTracklets(config, diaSourceTable, visitTable)
+        (dets, tracklets, trac2det) = map(pd.DataFrame, [dets, tracklets, trac2det])
 
         #Do something about trailed sources
-        
-        return lsst.pipe.base.Struct(trackletSources=trackout[0],
-                                     tracklets=trackout[1],
-                                     trk2det=trackout[2]
+        return lsst.pipe.base.Struct(trackletSources=dets,
+                                     tracklets=tracklets,
+                                     trk2det=trac2det
                                      )
     
